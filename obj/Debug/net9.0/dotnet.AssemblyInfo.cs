d63637f0693f5d075ab1f1edca3eec1a21d4ca80
Felix <66130644+art23132@users.noreply.github.com>
2025-11-14 13:01:34 +0500

    minor chage

--- obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0abea8c988ffc229d6705512faf733533bcaef5")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
